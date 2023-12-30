4773b21a0a8c75a968b2acaa6f82f52bbc602983
preritjn <preritbgr3010@gmail.com>
2023-12-30 13:33:47 +0530

    Added new page

--- React/dojo-blog/src/Home.js
+++ React/dojo-blog/src/Home.js
@@ -1,7 +1,36 @@
+import { useState } from "react";
+
 function Home() {
+    const [blogs, setBlogs] = useState([
+        {
+            title: 'My new website',
+            body: 'lorem ipsum...',
+            author: 'Prerit',
+            id: 1
+        },
+        {
+            title: 'Welcome Party!',
+            body: 'lorem ipsum...',
+            author: 'Kumar',
+            id: 2
+        },
+        {
+            title: 'Ole Ole',
+            body: 'lorem ipsum...',
+            author: 'Jain',
+            id: 3
+        },
+    ]);
     return (  
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog)=> {
+                return (
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written by {blog.author}</p>
+                    </div>
+                );
+            })}
         </div>
     );
 }
