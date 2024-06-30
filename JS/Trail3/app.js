29a19f18c472fe142261e9f03746f788877a0855
preritjn <preritbgr3010@gmail.com>
2024-06-30 14:42:36 +0530

    added some code

---
+++ JS/Trail3/app.js
@@ -0,0 +1,80 @@
+const playButton = document.getElementById('play')
+const pauseButton = document.getElementById('pause')
+const resetButton = document.getElementById('reset')
+
+playButton.addEventListener('click',(e) => {
+    if(!playButton.classList.contains('disabled')) {
+        e.target.classList.toggle('disabled')
+        e.target.parentElement.classList.toggle('disabled')
+    }    
+    if(pauseButton.classList.contains('disabled')) {
+        pauseButton.classList.toggle('disabled')
+        pauseButton.parentElement.classList.toggle('disabled')
+    }
+})
+
+pauseButton.addEventListener('click',(e) => {    
+    if(!pauseButton.classList.contains('disabled')) {
+        e.target.classList.toggle('disabled')
+        e.target.parentElement.classList.toggle('disabled')
+    }
+    if(playButton.classList.contains('disabled')) {
+        playButton.classList.toggle('disabled')
+        playButton.parentElement.classList.toggle('disabled')
+    }
+})
+
+resetButton.addEventListener('click',(e) => {
+    if(playButton.classList.contains('disabled')) {
+        playButton.classList.toggle('disabled')
+        playButton.parentElement.classList.toggle('disabled')
+    }
+    if(pauseButton.classList.contains('disabled')) {
+        pauseButton.classList.toggle('disabled')
+        pauseButton.parentElement.classList.toggle('disabled')
+    }
+    clearInterval(interval)
+    document.getElementById('timer').innerText = `00:00:00`
+})
+
+let seconds = 0
+let minutes = 0
+let hours = 0
+
+let leadingSeconds = 0
+let leadingMinutes = 0
+let leadingHours = 0
+
+function stopWatch() {
+    seconds++
+
+    if(seconds / 60 === 1){
+        seconds = 0
+        minutes++
+
+        if(minutes / 60 === 1){
+            minutes = 0
+            hours++
+        }
+    }
+
+    if(seconds < 10)
+        leadingSeconds = '0' + seconds.toString()
+    else
+        leadingSeconds = seconds
+    
+
+    if(minutes < 10)
+        leadingMinutes = '0' + minutes.toString()
+    else
+        leadingMinutes = minutes
+    
+
+    if(hours < 10)
+        leadingHours = '0' + hours.toString()
+    else
+        leadingHours = hours
+    document.getElementById('timer').innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`
+}
+
+const interval = window.setInterval(stopWatch,1000)
