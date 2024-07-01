466545e09a55c2a54f1b6deebc2cc4a9119fcaab
preritjn <preritbgr3010@gmail.com>
2024-07-01 09:57:49 +0530

    added final code

--- JS/Trail3/app.js
+++ JS/Trail3/app.js
@@ -1,42 +1,7 @@
 const playButton = document.getElementById('play')
 const pauseButton = document.getElementById('pause')
 const resetButton = document.getElementById('reset')
-
-playButton.addEventListener('click',(e) => {
-    if(!playButton.classList.contains('disabled')) {
-        e.target.classList.toggle('disabled')
-        e.target.parentElement.classList.toggle('disabled')
-    }    
-    if(pauseButton.classList.contains('disabled')) {
-        pauseButton.classList.toggle('disabled')
-        pauseButton.parentElement.classList.toggle('disabled')
-    }
-})
-
-pauseButton.addEventListener('click',(e) => {    
-    if(!pauseButton.classList.contains('disabled')) {
-        e.target.classList.toggle('disabled')
-        e.target.parentElement.classList.toggle('disabled')
-    }
-    if(playButton.classList.contains('disabled')) {
-        playButton.classList.toggle('disabled')
-        playButton.parentElement.classList.toggle('disabled')
-    }
-})
-
-resetButton.addEventListener('click',(e) => {
-    if(playButton.classList.contains('disabled')) {
-        playButton.classList.toggle('disabled')
-        playButton.parentElement.classList.toggle('disabled')
-    }
-    if(pauseButton.classList.contains('disabled')) {
-        pauseButton.classList.toggle('disabled')
-        pauseButton.parentElement.classList.toggle('disabled')
-    }
-    clearInterval(interval)
-    document.getElementById('timer').innerText = `00:00:00`
-})
-
+let interval = null
 let seconds = 0
 let minutes = 0
 let hours = 0
@@ -77,4 +42,43 @@ function stopWatch() {
     document.getElementById('timer').innerText = `${leadingHours}:${leadingMinutes}:${leadingSeconds}`
 }
 
-const interval = window.setInterval(stopWatch,1000)
+playButton.addEventListener('click',(e) => {
+    if(!playButton.classList.contains('disabled')) {
+        e.target.classList.toggle('disabled')
+        e.target.parentElement.classList.toggle('disabled')
+    }    
+    if(pauseButton.classList.contains('disabled')) {
+        pauseButton.classList.toggle('disabled')
+        pauseButton.parentElement.classList.toggle('disabled')
+    }
+    interval = window.setInterval(stopWatch,1000)
+})
+
+pauseButton.addEventListener('click',(e) => {    
+    if(!pauseButton.classList.contains('disabled')) {
+        e.target.classList.toggle('disabled')
+        e.target.parentElement.classList.toggle('disabled')
+    }
+    if(playButton.classList.contains('disabled')) {
+        playButton.classList.toggle('disabled')
+        playButton.parentElement.classList.toggle('disabled')
+    }
+    window.clearInterval(interval)
+})
+
+resetButton.addEventListener('click',(e) => {
+    if(playButton.classList.contains('disabled')) {
+        playButton.classList.toggle('disabled')
+        playButton.parentElement.classList.toggle('disabled')
+    }
+    if(!pauseButton.classList.contains('disabled')) {
+        pauseButton.classList.toggle('disabled')
+        pauseButton.parentElement.classList.toggle('disabled')
+    }
+    window.clearInterval(interval)
+    seconds = 0
+    minutes = 0
+    hours = 0
+    document.getElementById('timer').innerText = `00:00:00`
+})
+
