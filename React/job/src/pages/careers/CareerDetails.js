c8374bde056e9fe0fa73901d8c3c44425abfbba4
preritjn <preritbgr3010@gmail.com>
2024-07-04 23:48:09 +0530

    done some changes

--- React/job/src/pages/careers/CareerDetails.js
+++ React/job/src/pages/careers/CareerDetails.js
@@ -1,25 +1,23 @@
 import { useLoaderData } from "react-router-dom"
 
 export default function CareerDetails() {
-
-    const career = useLoaderData()
-
-    return (
-        <div className="career-details">
-            <h2>Career Details for {career.title}</h2>
-            <p>Starting Salary : {career.salary}</p>
-            <p>Location : {career.location}</p>
-            <div className="details">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magnam itaque natus nihil ipsam dignissimos eveniet voluptatem facilis recusandae autem, sit iusto quae nemo doloribus numquam minus ipsa odit laudantium.</p>
-            </div>
-        </div>
-    )
+  const career = useLoaderData()
+  return (
+    <div className="career-details">
+      <h2>Career Details for {career.title}</h2>
+      <p>Starting Salary : {career.salary}</p>
+      <p>Location : {career.location}</p>
+      <div className="details">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim magnam itaque natus nihil ipsam dignissimos eveniet voluptatem facilis recusandae autem, sit iusto quae nemo doloribus numquam minus ipsa odit laudantium.</p>
+      </div>
+    </div>
+  )
 }
 
 export const careerDetailsLoader = async ({params}) => {
-    const {id} = params
-    const response = await fetch('http://localhost:8000/careers/'+id)
-    if(!response.ok)
-        throw Error('Unable to fetch career details')
-    return response.json()
+  const {id} = params
+  const response = await fetch('http://localhost:8000/careers/'+id)
+  if(!response.ok)
+    throw Error('Unable to fetch career details')
+  return response.json()
 }
--- React/job/src/pages/careers/Careers.js
+++ React/job/src/pages/careers/Careers.js
@@ -1,22 +1,22 @@
 import { Link, useLoaderData } from "react-router-dom"
 
 export default function Careers() {
-    const careers = useLoaderData()
-    return (
-        <div className="careers">
-            {careers.map(career=>(
-                <Link to={career.id.toString()} key={career.id}>
-                    <p>{career.title}</p>
-                    <p>{career.location}</p>
-                </Link>
-            ))}
-        </div>
-    )
+  const careers = useLoaderData()
+  return (
+    <div className="careers">
+      {careers.map(career=>(
+        <Link to={career.id.toString()} key={career.id}>
+          <p>{career.title}</p>
+          <p>{career.location}</p>
+        </Link>
+        ))}
+    </div>
+  )
 }
 
 export const careersLoader = async () => {
-    const response = await fetch('http://localhost:8000/careers')
-    if(!response.ok)
-        throw Error('Unable to fetch careers')
-    return response.json()
+  const response = await fetch('http://localhost:8000/careers')
+  if(!response.ok)
+    throw Error('Unable to fetch careers')
+  return response.json()
 }
